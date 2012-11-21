5fc50e259094f0fc6f0236b6ab30e7829088b4bf
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-11-21 23:22:01 +0700

    UPGRADESS3: Massage code to be DataExtension compliant

--- code/FacebookMetadataSiteConfig.php
+++ code/FacebookMetadataSiteConfig.php
@@ -1,27 +1,22 @@
 <?php
   
-class FacebookMetadataSiteConfig extends DataObjectDecorator {
+class FacebookMetadataSiteConfig extends DataExtension {
+
+    static $db = array(
+        'SkipToMainContentAccessKey' => 'VarChar(1)'
+    );
+
+    static $has_one = array(
+        'FacebookLogo' => 'Image'
+    );
      
-    function extraStatics() {
-        return array(
-            'db' => array(
-                'SkipToMainContentAccessKey' => 'VarChar(1)'
-            ),
-            'has_one' => array(
-                'FacebookLogo' => 'Image'
-            )
-        );
-    }
   
-    public function updateCMSFields(FieldSet &$fields) {         
+    public function updateCMSFields(FieldList $fields) {         
 	    $tf2 = new TextField('SkipToMainContentAccessKey');
 	    $tf2->setMaxLength(1);
 	    $fields->addFieldToTab('Root.FacebookMetadata', $tf2);
 	    $fields->renameField("SkipToMainContentAccessKey", _t('AccessKey.SKIP_TO_MAIN_CONTENT_ACCESS_KEY'));
-
         $fields->addFieldToTab("Root.FacebookMetadata", new ImageField("FacebookLogo", _t('Facebook.METADATA_LOGO', 'Image that will show in facebook when linking to this site. The image should be a square')));
-        
-
 	}
 
  
