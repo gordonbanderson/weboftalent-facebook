f2e506ed1d65137edebe59ab026bf956838ea09a
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-01-16 20:46:28 +0700

    UPGRADE: Use upload field for image which is compatible with ss3

--- code/FacebookMetadataSiteConfig.php
+++ code/FacebookMetadataSiteConfig.php
@@ -16,7 +16,7 @@ class FacebookMetadataSiteConfig extends DataExtension {
 	    $tf2->setMaxLength(1);
 	    $fields->addFieldToTab('Root.FacebookMetadata', $tf2);
 	    $fields->renameField("SkipToMainContentAccessKey", _t('AccessKey.SKIP_TO_MAIN_CONTENT_ACCESS_KEY'));
-        $fields->addFieldToTab("Root.FacebookMetadata", new ImageField("FacebookLogo", _t('Facebook.METADATA_LOGO', 'Image that will show in facebook when linking to this site. The image should be a square')));
+        $fields->addFieldToTab("Root.FacebookMetadata", new UploadField("FacebookLogo", _t('Facebook.METADATA_LOGO', 'Image that will show in facebook when linking to this site. The image should be a square')));
 	}
 
  
