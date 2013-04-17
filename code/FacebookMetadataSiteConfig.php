e72c1e21ec397c18694d73eb6a104ba9d7f2eb11
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-04-17 15:47:05 +0700

    FIX: Remove empty field, hangover from access keys module

--- code/FacebookMetadataSiteConfig.php
+++ code/FacebookMetadataSiteConfig.php
@@ -12,11 +12,10 @@ class FacebookMetadataSiteConfig extends DataExtension {
      
   
     public function updateCMSFields(FieldList $fields) {         
-	    $tf2 = new TextField('SkipToMainContentAccessKey');
-	    $tf2->setMaxLength(1);
-	    $fields->addFieldToTab('Root.FacebookMetadata', $tf2);
+	 
 	    $fields->renameField("SkipToMainContentAccessKey", _t('AccessKey.SKIP_TO_MAIN_CONTENT_ACCESS_KEY'));
-        $fields->addFieldToTab("Root.FacebookMetadata", new UploadField("FacebookLogo", _t('Facebook.METADATA_LOGO', 'Image that will show in facebook when linking to this site. The image should be a square')));
+        $fields->addFieldToTab("Root.FacebookMetadata", new UploadField("FacebookLogo", _t('Facebook.METADATA_LOGO',
+            'Image that will show in facebook when linking to this site. The image should be a square of minimum size 200px')));
 	}
 
  
