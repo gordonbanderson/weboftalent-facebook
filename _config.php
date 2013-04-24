af12421bde6ad320a43157aa38d108cfd9b48cab
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-04-24 16:09:41 +0700

    ENHANCEMENT: Enable the new page extension for HTML entities

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
 <?php
 Object::add_extension('Page_Controller', 'FacebookCredentialsExtension');
+Object::add_extension('Page_Controller', 'FacebookPageExtension');
 DataObject::add_extension('SiteConfig', 'FacebookMetadataSiteConfig');
 ?>
